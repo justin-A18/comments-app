a7991d6480550fb7b8549558b95c7767f6a00075
Justin Huertas <justin.hv08@gmail.com>
2025-03-01 14:57:40 -0500

    feat: Limit number of fetched comments to 9

--- src/core/use-cases/comment/get-all-comments.use-case.ts
+++ src/core/use-cases/comment/get-all-comments.use-case.ts
@@ -3,7 +3,7 @@ import { HttpAdapter } from "@/config/adapters/http";
 import { CommentEntity } from "@/core/entity";
 
 export const getAllCommentsUseCase = async (apiFetcher: HttpAdapter) => {
-	const data = await apiFetcher.get<CommentEntity[]>('/comments');
+	const data = await apiFetcher.get<CommentEntity[]>('/comments?_limit=9');
 	return CommentMapper.toDomainArray(data);
 	;
 };
